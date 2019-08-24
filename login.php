cc307815eefd36e447c2d9576c4ce580a2200136
patrickdowling41 <patrick.dowling41@gmail.com>
2019-08-24 22:00:47 +1000

    Added form elements

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -11,27 +13,54 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
 
+    <!-- TODO import CSS -->
+
   </head>
 </html>
 
 <?php require('db_connect.php'); ?>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col col-lg">
-                <p>Test</p>
-            </div>
 
-            <div class="col col-lg">
-                <p>Test</p>
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+        </div>
+        <div class="col-lg-4">
+            <h1>Create a new account</h1>
+            <h3>It's quick and easy</h3>
 
-            <div class="col col-lg">
-                <p>Test</p>
-            </div>
+            <form>
+                <div class="inline">
+                    <input type="text" name="firstname-field" placeholder="First name"/>
+                    <input type="text" name="surname-field" placeholder="Surname"/>
+                </div><br>
+                <input type="text" name="email-field" placeholder="Email"/><br>
+                <input type="text" name="password-field" placeholder="New password"/><br>
+                <div class="title-tag">Birthday</div>
+                <select>
+                    <option value="Select Day" disabled>Select Day</option>
+                    <?php
+                        for ($day = 1; $day<= 31; $day++)
+                        {
+                            echo '<option value="'.$day.'">'.$day.'</option>';
+                        }
+                    ?>
+               
+                </select>
+                <div class="title-tag">Gender</div>
+                <div class="inline">
+                    <input type="radio" name="gender" value="male"> Male<br>
+                    <input type="radio" name="gender" value="female"> Female<br>
+                </div>
+
+            </form> 
+
+        </div>
+
+
         </div>
     </div>
+</div>
 
 </body>
-
