41cdb2cc04f4f14eb85362674621c3bfe2ba7bc0
patrickdowling41 <patrick.dowling41@gmail.com>
2019-08-27 20:46:21 +1000

    Added timestamp to query

--- app/createPost.php
+++ app/createPost.php
@@ -4,21 +4,26 @@ session_start();
 
 $body = $_POST['cp-body'];
 $email = $$_COOKIE['email'];
+date_default_timezone_set('Australia/Melbourne');
+$timeOfPost = date('d-m-Y h:i:s');
 
 $addLike = 'INSERT INTO RATING (
     body,
-    posterEmail
+    posterEmail,
+    timeOfPost
 )
 VALUES
 (
     :bv_body,
-    :bv_email
+    :bv_email,
+    bv_timeOfPost
 )';
 
 $stid = oci_parse($conn, $addLike);
 
 oci_bind_by_name($stid, ":bv_body", $body);
 oci_bind_by_name($stid, ":bv_email", $email);
+oci_bind_by_name($stid, "bv_timeOfPost", $timeOfPost);
 
 header("Location: index.php");
 
